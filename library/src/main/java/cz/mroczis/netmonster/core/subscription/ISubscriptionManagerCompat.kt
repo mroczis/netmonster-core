cbdf51c94ffdbd3400e57de9e53164f4572b2277
Michal Mroček <mroczis@gmail.com>
2020-01-01 16:45:11 +0800

    More precise documentation for ISubscriptionManagerCompat

--- library/src/main/java/cz/mroczis/netmonster/core/subscription/ISubscriptionManagerCompat.kt
+++ library/src/main/java/cz/mroczis/netmonster/core/subscription/ISubscriptionManagerCompat.kt
@@ -8,7 +8,15 @@ import cz.mroczis.netmonster.core.model.annotation.SinceSdk
 interface ISubscriptionManagerCompat {
 
     /**
-     * Retries all unique ids of active subscriptions
+     * Retrieves all unique ids of active subscriptions.
+     *
+     * By active is meant that:
+     *  - SIM is inserted and ready to be used
+     *  - eSIM is activated
+     *
+     * Resulting list is never empty, in case if subscription id is unknown
+     * or unsupported on current Android version [Int.MAX_VALUE] is the only
+     * value in the list.
      */
     @SinceSdk(Build.VERSION_CODES.LOLLIPOP_MR1)
     @RequiresPermission(Manifest.permission.READ_PHONE_STATE)
--- library/src/main/java/cz/mroczis/netmonster/core/subscription/SubscriptionManagerCompat22.kt
+++ library/src/main/java/cz/mroczis/netmonster/core/subscription/SubscriptionManagerCompat22.kt
@@ -5,7 +5,9 @@ import android.annotation.TargetApi
 import android.content.Context
 import android.os.Build
 import android.telephony.SubscriptionManager
+import android.telephony.TelephonyManager
 import androidx.annotation.RequiresPermission
+import cz.mroczis.netmonster.core.factory.NetMonsterFactory
 
 @TargetApi(Build.VERSION_CODES.LOLLIPOP_MR1)
 internal open class SubscriptionManagerCompat22(
@@ -16,7 +18,24 @@ internal open class SubscriptionManagerCompat22(
         context.getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE) as SubscriptionManager
 
     @RequiresPermission(Manifest.permission.READ_PHONE_STATE)
-    override fun getActiveSubscriptionIds(): List<Int> =
-        manager.activeSubscriptionInfoList?.map { it.subscriptionId } ?: emptyList()
+    override fun getActiveSubscriptionIds(): List<Int> {
+        val subscriptions = manager.activeSubscriptionInfoList?.mapNotNull {
+            val id = it.subscriptionId
+
+            // Samsung phones report deactivated SIMs in list of active subscriptions
+            // Explicit check if SIM is ready is required in order to not show obsolete data
+            if (NetMonsterFactory.getTelephony(context, id).getTelephonyManager()?.simState == TelephonyManager.SIM_STATE_READY) {
+                id
+            } else {
+                null
+            }
+        }
+
+        return if (subscriptions.isNullOrEmpty()) {
+            super.getActiveSubscriptionIds()
+        } else {
+            subscriptions
+        }
+    }
 
 }
