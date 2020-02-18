26c530560fe2e30a2267d6f7ad429ef3c06864fc
Michal Mroček <mroczis@gmail.com>
2020-02-18 22:17:27 +0100

    Added filtration of GSM cells for Pixel devices

--- library/src/main/java/cz/mroczis/netmonster/core/feature/postprocess/InvalidCellsPostprocessor.kt
+++ library/src/main/java/cz/mroczis/netmonster/core/feature/postprocess/InvalidCellsPostprocessor.kt
@@ -1,5 +1,6 @@
 package cz.mroczis.netmonster.core.feature.postprocess
 
+import cz.mroczis.netmonster.core.model.cell.CellGsm
 import cz.mroczis.netmonster.core.model.cell.CellLte
 import cz.mroczis.netmonster.core.model.cell.ICell
 
@@ -13,6 +14,8 @@ class InvalidCellsPostprocessor : ICellPostprocessor {
     override fun postprocess(list: List<ICell>): List<ICell> = list.toMutableList().asSequence()
         // Google Pixel phones in LTE use this to say that cell is nearby but it's data will appear in a near future
         .filterNot { it is CellLte && it.pci == 0 && it.signal.rssi == -51 && it.signal.rsrp == null && it.signal.rsrq == null  }
+        // Google Pixel phones in GSM when they are loosing signal return GSM cells just with 0 ARFCN and 0 BSIC
+        .filterNot { it is CellGsm && it.bsic == 0 && it.band?.arfcn == 0 && it.signal.rssi == null && it.cid == null }
         .toList()
 
 }
