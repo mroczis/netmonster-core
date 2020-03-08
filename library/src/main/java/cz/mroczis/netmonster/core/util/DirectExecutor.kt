32f4bdc374be47cc3f7bf145468e12cd404a5173
Michal Mroček <mroczis@gmail.com>
2020-03-08 23:01:22 +0100

    Fix - crash from AOSP when delivering updates of CellInfo

--- library/src/main/java/cz/mroczis/netmonster/core/util/DirectExecutor.kt
+++ library/src/main/java/cz/mroczis/netmonster/core/util/DirectExecutor.kt
@@ -5,6 +5,14 @@ import java.util.concurrent.Executor
 /**
  * Executes [Runnable] directly on current thread.
  */
-class DirectExecutor : Executor {
-    override fun execute(runnable: Runnable) = runnable.run()
+internal class DirectExecutor : Executor {
+    override fun execute(runnable: Runnable) {
+        try {
+            runnable.run()
+        } catch (e: NullPointerException) {
+            // Bug in AOSP - TelephonyManager#requestCellInfoUpdate() throws NullPointerException
+            // when an error during update occurs and native code does not pass an exception
+            // -> Attempt to invoke virtual method 'java.lang.Throwable android.os.ParcelableException.getCause()' on a null object reference
+        }
+    }
 }
