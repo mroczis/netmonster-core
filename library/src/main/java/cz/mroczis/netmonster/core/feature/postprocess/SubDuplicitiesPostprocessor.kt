cc9dc6e345b1d16c0ffab765db2b4dbdd97aed81
Michal Mroček <mroczis@gmail.com>
2020-03-10 18:34:13 +0100

    Subscription duplicities postprocessor excluded above Q+

--- library/src/main/java/cz/mroczis/netmonster/core/feature/postprocess/SubDuplicitiesPostprocessor.kt
+++ library/src/main/java/cz/mroczis/netmonster/core/feature/postprocess/SubDuplicitiesPostprocessor.kt
@@ -1,6 +1,7 @@
 package cz.mroczis.netmonster.core.feature.postprocess
 
 import android.Manifest
+import android.os.Build
 import androidx.annotation.RequiresPermission
 import cz.mroczis.netmonster.core.model.Network
 import cz.mroczis.netmonster.core.model.cell.ICell
@@ -27,66 +28,71 @@ class SubDuplicitiesPostprocessor(
 ) : ICellPostprocessor {
 
     @RequiresPermission(allOf = [Manifest.permission.READ_PHONE_STATE, Manifest.permission.ACCESS_COARSE_LOCATION])
-    override fun postprocess(list: List<ICell>): List<ICell> {
-        val subscriptionsIds = subscription.getActiveSubscriptionIds()
-
-        return if (subscriptionsIds.size <= 1) {
-            list // Not applicable for single SIM devices
+    override fun postprocess(list: List<ICell>): List<ICell> =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            list
         } else {
-            val subCells = list.groupBy { it.subscriptionId }
+            val subscriptionsIds = subscription.getActiveSubscriptionIds()
 
-            subscriptionsIds
-                .map { subId ->
-                    val currentNetwork = networkOperatorGetter.invoke(subId)
+            if (subscriptionsIds.size <= 1) {
+                // Not applicable for single SIM devices
+                // and on Android 10+ cause delivered results are always connected to subscription id
+                // so we do not need to filter duplicates
+                list
+            } else {
+                val subCells = list.groupBy { it.subscriptionId }
 
-                    // Cells assigned to subscription id for current iteration
-                    val cells = subCells[subId] ?: emptyList()
+                subscriptionsIds
+                    .map { subId ->
+                        val currentNetwork = networkOperatorGetter.invoke(subId)
 
-                    // Positions of cells that are marked as serving
-                    val indexesOfPrimaryCells = cells.mapIndexedNotNull { i, cell ->
-                        if (cell.connectionStatus is PrimaryConnection) {
-                            i
-                        } else null
-                    }
+                        // Cells assigned to subscription id for current iteration
+                        val cells = subCells[subId] ?: emptyList()
 
-                    // Cell that has the same network & subscription id
-                    // Indexes in 'cells' variable where subscription id and operators match
-                    val matchingIndexes = indexesOfPrimaryCells.filter { index ->
-                        cells[index].network == currentNetwork && cells[index].subscriptionId == subId
-                    }
-
-                    // Now let's get interval for sublist that we'll extract
-                    // Start = place where is serving cell with correct network & sub id
-                    // End = place where's next serving cell (exclusive) or end of the list if there's none
-                    val start: Int? = when {
-                        matchingIndexes.size == 1 -> {
-                            // Usual situation, dual SIM, two different PLMNs
-                            matchingIndexes[0]
+                        // Positions of cells that are marked as serving
+                        val indexesOfPrimaryCells = cells.mapIndexedNotNull { i, cell ->
+                            if (cell.connectionStatus is PrimaryConnection) {
+                                i
+                            } else null
                         }
-                        matchingIndexes.size > 1 -> {
-                            // Case: user has more SIM cards that are connected to same network
-                            // In this case we grab index of subscription that SHOULD match with position
-                            // of serving cell
-                            matchingIndexes.getOrNull(subscriptionsIds.indexOf(subId))
+
+                        // Cell that has the same network & subscription id
+                        // Indexes in 'cells' variable where subscription id and operators match
+                        val matchingIndexes = indexesOfPrimaryCells.filter { index ->
+                            cells[index].network == currentNetwork && cells[index].subscriptionId == subId
                         }
-                        else -> null
-                    }
-                    val end: Int? = if (start != null) {
-                        // End = where next serving cell is or at the end of the list
-                        indexesOfPrimaryCells.firstOrNull { it > start } ?: cells.size
-                    } else {
-                        null
-                    }
 
-                    if (start != null && end != null) {
-                        cells.subList(start, end)
-                    } else {
-                        emptyList()
-                    }
+                        // Now let's get interval for sublist that we'll extract
+                        // Start = place where is serving cell with correct network & sub id
+                        // End = place where's next serving cell (exclusive) or end of the list if there's none
+                        val start: Int? = when {
+                            matchingIndexes.size == 1 -> {
+                                // Usual situation, dual SIM, two different PLMNs
+                                matchingIndexes[0]
+                            }
+                            matchingIndexes.size > 1 -> {
+                                // Case: user has more SIM cards that are connected to same network
+                                // In this case we grab index of subscription that SHOULD match with position
+                                // of serving cell
+                                matchingIndexes.getOrNull(subscriptionsIds.indexOf(subId))
+                            }
+                            else -> null
+                        }
+                        val end: Int? = if (start != null) {
+                            // End = where next serving cell is or at the end of the list
+                            indexesOfPrimaryCells.firstOrNull { it > start } ?: cells.size
+                        } else {
+                            null
+                        }
 
-                }.flatten()
+                        if (start != null && end != null) {
+                            cells.subList(start, end)
+                        } else {
+                            emptyList()
+                        }
+
+                    }.flatten()
+            }
         }
-
-    }
 
 }
