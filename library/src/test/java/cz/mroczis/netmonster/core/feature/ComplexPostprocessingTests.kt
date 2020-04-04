e6575eb54568050c1f20845a5c7d83769e4f54a7
Michal Mroček <mroczis@gmail.com>
2020-04-04 20:54:39 +0200

    Android gradle 3.6.2 + Kotlin 1.3.71

--- library/src/test/java/cz/mroczis/netmonster/core/feature/ComplexPostprocessingTests.kt
+++ library/src/test/java/cz/mroczis/netmonster/core/feature/ComplexPostprocessingTests.kt
@@ -259,4 +259,135 @@ class ComplexPostprocessingTests : StringSpec({
             get(3)!!.size shouldBe 3
         }
     }
+
+    "Dual SIM, Android 10, both having neighbouring cells, different technologies, same newtwork" {
+        val cells = mutableListOf<ICell>().apply {
+            add(
+                CellGsm(
+                    network = Network.map(203, 3),
+                    cid = 4234,
+                    lac = 1231,
+                    bsic = 9,
+                    band = BandTableGsm.map(114, "203"),
+                    signal = SignalGsm(
+                        rssi = -81,
+                        bitErrorRate = null,
+                        timingAdvance = null
+                    ),
+                    subscriptionId = 1,
+                    connectionStatus = PrimaryConnection()
+                )
+            )
+
+            add(
+                CellGsm(
+                    network = Network.map(203, 3),
+                    cid = 4673,
+                    lac = 1231,
+                    bsic = 8,
+                    band = BandTableGsm.map(46, "203"),
+                    signal = SignalGsm(
+                        rssi = -93,
+                        bitErrorRate = null,
+                        timingAdvance = null
+                    ),
+                    subscriptionId = 1,
+                    connectionStatus = NoneConnection()
+                )
+            )
+
+            add(
+                CellLte(
+                    network = Network.map(203, 3),
+                    eci = 213733608,
+                    tac = 29230,
+                    pci = 333,
+                    band = BandTableLte.map(6200),
+                    signal = SignalLte(
+                        rssi = -77,
+                        rsrp = -101.0,
+                        rsrq = -6.0,
+                        snr = null,
+                        cqi = null,
+                        timingAdvance = null
+                    ),
+                    bandwidth = null,
+                    subscriptionId = 3,
+                    connectionStatus = PrimaryConnection()
+                )
+            )
+
+            add(
+                CellLte(
+                    network = Network.map(203, 3),
+                    eci = null,
+                    tac = null,
+                    pci = 334,
+                    band = null,
+                    signal = SignalLte(
+                        rssi = -91,
+                        rsrp = -116.0,
+                        rsrq = -16.0,
+                        snr = null,
+                        cqi = null,
+                        timingAdvance = null
+                    ),
+                    bandwidth = null,
+                    subscriptionId = 3,
+                    connectionStatus = NoneConnection()
+                )
+            )
+
+            add(
+                CellGsm(
+                    network = Network.map(203, 3),
+                    cid = null,
+                    lac = null,
+                    bsic = 255,
+                    band = BandTableGsm.map(0, "203"),
+                    signal = SignalGsm(
+                        rssi = -113,
+                        bitErrorRate = null,
+                        timingAdvance = null
+                    ),
+                    subscriptionId = 3,
+                    connectionStatus = NoneConnection()
+                )
+            )
+        }
+
+        val subManager = object : ISubscriptionManagerCompat {
+            override fun getActiveSubscriptionIds(): List<Int> =
+                getActiveSubscriptions().map { it.subscriptionId }
+
+            override fun getActiveSubscriptions(): List<SubscribedNetwork> =
+                listOf(
+                    SubscribedNetwork(1, Network.map(203, 3)),
+                    SubscribedNetwork(3, Network.map(203, 3))
+                )
+        }
+
+        val serviceStateSimulation: (Int) -> Network? = { subId ->
+            subManager.getActiveSubscriptions().first { it.subscriptionId == subId }.network
+        }
+
+        val postprocessors = listOf(
+            MocnNetworkPostprocessor(subManager, serviceStateSimulation),
+            InvalidCellsPostprocessor(),
+            PrimaryCellPostprocessor(),
+            SubDuplicitiesPostprocessor(subManager, serviceStateSimulation),
+            PlmnPostprocessor()
+        )
+
+        var res : List<ICell> = cells
+        postprocessors.forEach { res = it.postprocess(res) }
+
+        // Postprocessors should be able to identify and separate subscription ids properly
+        res.size shouldBe 5
+        res.groupBy { it.subscriptionId }.apply {
+            size shouldBe 2
+            get(1)!!.size shouldBe 2
+            get(3)!!.size shouldBe 3
+        }
+    }
 })
