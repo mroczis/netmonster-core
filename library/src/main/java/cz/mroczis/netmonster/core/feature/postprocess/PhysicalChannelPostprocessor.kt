130af3364efeff01fc2efecaf7a9e77cb8505879
Michal Mroček <mroczis@gmail.com>
2020-04-24 23:44:54 +0200

    Improvements for assigning bandwidth to ICell

--- library/src/main/java/cz/mroczis/netmonster/core/feature/postprocess/PhysicalChannelPostprocessor.kt
+++ library/src/main/java/cz/mroczis/netmonster/core/feature/postprocess/PhysicalChannelPostprocessor.kt
@@ -10,6 +10,16 @@ import cz.mroczis.netmonster.core.model.connection.PrimaryConnection
 import cz.mroczis.netmonster.core.model.connection.SecondaryConnection
 
 /**
+ * EARFCN for LTE
+ */
+private typealias Channel = Int
+
+/**
+ * Bandwidth in kHz
+ */
+private typealias Bandwidth = Int
+
+/**
  * Enriches data, works only for LTE.
  *
  * Basic abilities:
@@ -29,42 +39,150 @@ class PhysicalChannelPostprocessor(
                         .invoke(subId)
                         .toMutableList()
 
-                    cells.map { cell ->
-                        if (cell is CellLte) {
-                            var resultCell: CellLte = cell
+                    val res = MergeBundle(cells, configs)
+                        .removeRedundantConfigs()
+                        .mergeByPci()
+                        .mergeByPciWithHints()
+                        .mergeByPci()
+                        .mergeByConnection()
 
-                            // Round 1 - we care just about PCI which is supported in newer AOSP
-                            // versions, here we also get rid of cells that are already used
-                            val iterator = configs.iterator()
-                            while (iterator.hasNext()) {
-                                val config = iterator.next()
-                                val modifiedCell = mergeByCode(resultCell, config)
-
-                                if (modifiedCell != null) {
-                                    resultCell = modifiedCell
-                                    iterator.remove()
-                                }
-                            }
-
-                            // Round 2 - if there's something left we will try to merge it using
-                            // connectionStatus
-                            for (config in configs) {
-                                val modifiedCell = mergeByConnection(resultCell, config)
-                                if (modifiedCell != null) {
-                                    resultCell = modifiedCell
-                                }
-                            }
-
-                            resultCell
-                        } else {
-                            cell
-                        }
-                    }
+                    res.cells
 
                 }.flatten()
         } else {
             list
         }
+
+    /**
+     * Filtration round - remove configs that are useless. 
+     * This is only applicable for [PrimaryConnection] cells who already have bandwidth.
+     */
+    private fun MergeBundle.removeRedundantConfigs() : MergeBundle {
+        val usableConfigs = configs.filterNot { config ->
+            cells.find { it is CellLte && it.connectionStatus == config.connectionStatus && it.bandwidth == config.bandwidth && (it.pci == config.pci || config.pci == null) } != null
+        }
+        
+        return copy(configs = usableConfigs)
+    }
+
+    /**
+     * We will try to find unique PCI in both cell list and configs list.
+     * If we have that match then we'll merge data together.
+     */
+    private fun MergeBundle.mergeByPci(): MergeBundle {
+        val cellToConfig = configs
+            .filter { it.pci != null }
+            .mapNotNull { config ->
+                val candidates = cells
+                    .filterIsInstance(CellLte::class.java)
+                    .filter { cell -> cell.pci == config.pci && cell.bandwidth == null}
+
+                if (candidates.size == 1) {
+                    candidates[0] to config
+                } else {
+                    null
+                }
+            }.toMap()
+
+        val mergedCells = cells.map { cell ->
+            cellToConfig[cell]?.let { config ->
+                mergeByCode(cell as CellLte, config)
+            } ?: cell
+        }
+
+        val remainingConfigs = configs.toMutableList().apply {
+            removeAll(cellToConfig.values)
+        }.toList()
+
+        return MergeBundle(
+            cells = mergedCells,
+            configs = remainingConfigs
+        )
+    }
+
+    /**
+     * Here we handle case when one PCI is used multiple times in different channels.
+     * This method assumes that operator uses same bandwidth for given channel number which is
+     * in most cases true.
+     *
+     * Example
+     * -------
+     * Cells:
+     *  - A ... PCI = 10, EARFCN = 1850, BW = null
+     *  - B ... PCI = 10, EARFCN = 6400, BW = null
+     *  - C ... PCI = 20, EARFCN = 1850, BW = 20_000
+     *
+     * Configs:
+     *  - X ... PCI = 10, BW = 10_000
+     *  - Y ... PCI = 10, BW = 20_000
+     *
+     * Expected result is that Config Y will be merged with Cell A cause Cell C already has
+     * BW filled and shares same EARFCN with Cell S
+     */
+    private fun MergeBundle.mergeByPciWithHints(): MergeBundle {
+        val hints = bandwidthHints
+        val cellToConfig = cells
+            .filterIsInstance(CellLte::class.java)
+            .mapNotNull { cell ->
+                cell.band?.downlinkEarfcn
+                    ?.let { hints[it] } // Get bandwidth that fits the most
+                    ?.let { preferredBandwidth ->
+                        val candidateConfigs =
+                            configs.filter { it.bandwidth == preferredBandwidth && it.pci == cell.pci }
+
+                        if (candidateConfigs.size == 1) {
+                            cell to candidateConfigs[0]
+                        } else {
+                            null
+                        }
+                    }
+            }.toMap()
+
+        val mergedCells = cells.map { cell ->
+            cellToConfig[cell]?.let { config ->
+                mergeByCode(cell as CellLte, config)
+            } ?: cell
+        }
+
+        val remainingConfigs = configs.toMutableList().apply {
+            removeAll(cellToConfig.values)
+        }.toList()
+
+        return MergeBundle(
+            cells = mergedCells,
+            configs = remainingConfigs
+        )
+    }
+
+    /**
+     * Connection merging.
+     * PCI is completely ignored since it's not available or it was already used in previous iterations.
+     * Here we try ot match types of connections
+     */
+    private fun MergeBundle.mergeByConnection(): MergeBundle {
+        val primaryConfigs = configs.filter { it.connectionStatus is PrimaryConnection }
+        return if (primaryConfigs.size == 1) {
+            val config = primaryConfigs[0]
+            val merged = cells.map {
+                if (it is CellLte && it.connectionStatus is PrimaryConnection) {
+                    it.copy(bandwidth = it.bandwidth ?: config.bandwidth)
+                } else {
+                    it
+                }
+            }
+
+            MergeBundle(
+                cells = merged,
+                configs = configs.toMutableList().apply {
+                    remove(config)
+                }.toList()
+            )
+        } else {
+            // Cannot reliably handle multiple primary configs
+            // Just skip this merging
+            this
+        }
+    }
 
     /**
      * Merging by PCI, works since Android Q.
@@ -74,52 +192,52 @@ class PhysicalChannelPostprocessor(
      * the issue
      *  - Some phones also indicate secondary connection to some cells, we take advantage of that
      */
-    private fun mergeByCode(cell: CellLte, config: PhysicalChannelConfig): CellLte? =
-        if (cell.pci == config.pci) {
-            val connection =
-                if (cell.connectionStatus is PrimaryConnection) {
-                    cell.connectionStatus // Keep primary
-                } else if (config.connectionStatus is SecondaryConnection) {
-                    config.connectionStatus // Switch to secondary as reported by PhysicalChannel
-                } else if (cell.connectionStatus is NoneConnection) {
-                    // Samsung reports via PCC Primary connection for secondary cells, at least
-                    // that's what I saw on multiple devices, so keep it as a guess
-                    SecondaryConnection(isGuess = config.connectionStatus is PrimaryConnection)
-                } else {
-                    // Otherwise just keep it as it was
-                    cell.connectionStatus
-                }
+    private fun mergeByCode(cell: CellLte, config: PhysicalChannelConfig): CellLte {
+        val connection = when {
+            // Keep primary
+            cell.connectionStatus is PrimaryConnection ->
+                cell.connectionStatus
 
-            cell.copy(
-                bandwidth = cell.bandwidth ?: config.bandwidth,
-                connectionStatus = connection
-            )
-        } else {
-            null
+            // Switch to secondary as reported by PhysicalChannel
+            config.connectionStatus is SecondaryConnection ->
+                config.connectionStatus
+
+            // Samsung reports via PCC Primary connection for secondary cells, at least
+            // that's what I saw on multiple devices, so keep it as a guess
+            cell.connectionStatus is NoneConnection ->
+                SecondaryConnection(isGuess = config.connectionStatus is PrimaryConnection)
+
+            // Otherwise just keep it as it was
+            else -> cell.connectionStatus
         }
 
+        return cell.copy(
+            bandwidth = cell.bandwidth ?: config.bandwidth,
+            connectionStatus = connection
+        )
+    }
+
     /**
-     * Merging by ConnectionStatus, works since P.
-     *
-     * Attempts to merge [cell] with [config].
-     *  - Some phones do not return valid bandwidth using standard API, this method will attempt to fix
-     * the issue
-     *  - Some phones also indicate secondary connection to some cells, we take advantage of that
+     * Internal structure for merging
      */
-    private fun mergeByConnection(cell: CellLte, config: PhysicalChannelConfig): CellLte? =
-        if (cell.bandwidth == null) {
-            if (cell.connectionStatus == config.connectionStatus && cell.connectionStatus is PrimaryConnection) {
-                // We can add bandwidth if it's not filled yet
-                cell.copy(bandwidth = cell.bandwidth ?: config.bandwidth)
-            } else if (cell.connectionStatus !is PrimaryConnection && config.connectionStatus !is PrimaryConnection && cell.pci == config.pci) {
-                // PhysicalChannelConfig is the first source that is able to identify SecondaryConnections
-                // since Android 9
-                cell.copy(
-                    bandwidth = cell.bandwidth ?: config.bandwidth,
-                    connectionStatus = config.connectionStatus
-                )
-            } else null
-        } else null
-
-
+    private data class MergeBundle(
+        /**
+         * All cells that were scanned
+         */
+        val cells: List<ICell>,
+        /**
+         * Candidates that we want to use to enrich data in [cells]
+         */
+        val configs: List<PhysicalChannelConfig>
+    ) {
+        /**
+         * Helper map that gives us information about already assigned bandwidths
+         */
+        val bandwidthHints: Map<Channel, Bandwidth>
+            get() = cells.mapNotNull {
+                if (it is CellLte && it.bandwidth != null && it.band?.channelNumber != null) {
+                    it.band.channelNumber to it.bandwidth
+                } else null
+            }.toMap()
+    }
 }
---
+++ library/src/test/java/cz/mroczis/netmonster/core/feature/postprocess/PhysicalChannelPostprocessorTest.kt
@@ -0,0 +1,106 @@
+package cz.mroczis.netmonster.core.feature.postprocess
+
+import android.os.Build
+import cz.mroczis.netmonster.core.SdkTest
+import cz.mroczis.netmonster.core.db.BandTableLte
+import cz.mroczis.netmonster.core.model.cell.CellLte
+import cz.mroczis.netmonster.core.model.config.PhysicalChannelConfig
+import cz.mroczis.netmonster.core.model.connection.NoneConnection
+import cz.mroczis.netmonster.core.model.connection.PrimaryConnection
+import cz.mroczis.netmonster.core.model.connection.SecondaryConnection
+import cz.mroczis.netmonster.core.model.signal.SignalLte
+import io.kotlintest.shouldBe
+
+class PhysicalChannelPostprocessorTest : SdkTest(Build.VERSION_CODES.Q){
+
+    companion object {
+        private val SUB_ID = 1
+        private val LTE_SIGNAL = SignalLte(null, null, null, null, null, null)
+    }
+
+    init {
+
+        "Same PCIs twice, non-transitive" {
+            val pccProvider : (Int) -> List<PhysicalChannelConfig> =  { _ ->
+                listOf(
+                    PhysicalChannelConfig(connectionStatus = PrimaryConnection(), bandwidth = 20_000, channelNumber = null, pci = 149),
+                    PhysicalChannelConfig(connectionStatus = SecondaryConnection(isGuess = false), bandwidth = 20_000, channelNumber = null, pci = 149)
+                )
+            }
+
+            val cells = listOf(
+                CellLte(connectionStatus = PrimaryConnection(), eci = 1, tac = 1, pci = 149, band = BandTableLte.map(2950), bandwidth = 20_000, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null),
+                CellLte(connectionStatus = NoneConnection(), eci = null, tac = null, pci = 149, band = BandTableLte.map(1275), bandwidth = null, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null),
+                CellLte(connectionStatus = NoneConnection(), eci = null, tac = null, pci = 224, band = BandTableLte.map(1275), bandwidth = null, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null),
+                CellLte(connectionStatus = NoneConnection(), eci = null, tac = null, pci = 63, band = BandTableLte.map(2950), bandwidth = null, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null)
+            )
+
+            val result = PhysicalChannelPostprocessor(pccProvider).postprocess(cells)
+
+            val expected = listOf(
+                CellLte(connectionStatus = PrimaryConnection(), eci = 1, tac = 1, pci = 149, band = BandTableLte.map(2950), bandwidth = 20_000, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null),
+                CellLte(connectionStatus = SecondaryConnection(isGuess = false), eci = null, tac = null, pci = 149, band = BandTableLte.map(1275), bandwidth = 20_000, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null),
+                CellLte(connectionStatus = NoneConnection(), eci = null, tac = null, pci = 224, band = BandTableLte.map(1275), bandwidth = null, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null),
+                CellLte(connectionStatus = NoneConnection(), eci = null, tac = null, pci = 63, band = BandTableLte.map(2950), bandwidth = null, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null)
+            )
+
+            result shouldBe expected
+        }
+
+        "Same PCI twice, transitive through EARFCN" {
+            val pccProvider : (Int) -> List<PhysicalChannelConfig> =  { _ ->
+                listOf(
+                    PhysicalChannelConfig(connectionStatus = PrimaryConnection(), bandwidth = 20_000, channelNumber = null, pci = 149),
+                    PhysicalChannelConfig(connectionStatus = SecondaryConnection(isGuess = false), bandwidth = 10_000, channelNumber = null, pci = 149),
+                    PhysicalChannelConfig(connectionStatus = SecondaryConnection(isGuess = false), bandwidth = 20_000, channelNumber = null, pci = 63)
+                )
+            }
+
+            val cells = listOf(
+                CellLte(connectionStatus = PrimaryConnection(), eci = 1, tac = 1, pci = 149, band = BandTableLte.map(2950), bandwidth = null, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null),
+                CellLte(connectionStatus = NoneConnection(), eci = null, tac = null, pci = 149, band = BandTableLte.map(1275), bandwidth = null, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null),
+                CellLte(connectionStatus = NoneConnection(), eci = null, tac = null, pci = 224, band = BandTableLte.map(1275), bandwidth = null, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null),
+                CellLte(connectionStatus = NoneConnection(), eci = null, tac = null, pci = 63, band = BandTableLte.map(2950), bandwidth = null, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null)
+            )
+
+            val result = PhysicalChannelPostprocessor(pccProvider).postprocess(cells)
+
+            val expected = listOf(
+                CellLte(connectionStatus = PrimaryConnection(), eci = 1, tac = 1, pci = 149, band = BandTableLte.map(2950), bandwidth = 20_000, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null),
+                CellLte(connectionStatus = SecondaryConnection(isGuess = false), eci = null, tac = null, pci = 149, band = BandTableLte.map(1275), bandwidth = 10_000, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null),
+                CellLte(connectionStatus = NoneConnection(), eci = null, tac = null, pci = 224, band = BandTableLte.map(1275), bandwidth = null, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null),
+                CellLte(connectionStatus = SecondaryConnection(isGuess = false), eci = null, tac = null, pci = 63, band = BandTableLte.map(2950), bandwidth = 20_000, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null)
+            )
+
+            result shouldBe expected
+        }
+
+        "No PCI" {
+            val pccProvider : (Int) -> List<PhysicalChannelConfig> =  { _ ->
+                listOf(
+                    PhysicalChannelConfig(connectionStatus = PrimaryConnection(), bandwidth = 20_000, channelNumber = null, pci = null),
+                    PhysicalChannelConfig(connectionStatus = SecondaryConnection(isGuess = false), bandwidth = 10_000, channelNumber = null, pci = null)
+                )
+            }
+
+            val cells = listOf(
+                CellLte(connectionStatus = PrimaryConnection(), eci = 1, tac = 1, pci = 149, band = BandTableLte.map(2950), bandwidth = null, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null),
+                CellLte(connectionStatus = NoneConnection(), eci = null, tac = null, pci = 149, band = BandTableLte.map(1275), bandwidth = null, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null),
+                CellLte(connectionStatus = NoneConnection(), eci = null, tac = null, pci = 224, band = BandTableLte.map(1275), bandwidth = null, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null),
+                CellLte(connectionStatus = NoneConnection(), eci = null, tac = null, pci = 63, band = BandTableLte.map(2950), bandwidth = null, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null)
+            )
+
+            val result = PhysicalChannelPostprocessor(pccProvider).postprocess(cells)
+
+            val expected = listOf(
+                CellLte(connectionStatus = PrimaryConnection(), eci = 1, tac = 1, pci = 149, band = BandTableLte.map(2950), bandwidth = 20_000, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null),
+                CellLte(connectionStatus = NoneConnection(), eci = null, tac = null, pci = 149, band = BandTableLte.map(1275), bandwidth = null, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null),
+                CellLte(connectionStatus = NoneConnection(), eci = null, tac = null, pci = 224, band = BandTableLte.map(1275), bandwidth = null, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null),
+                CellLte(connectionStatus = NoneConnection(), eci = null, tac = null, pci = 63, band = BandTableLte.map(2950), bandwidth = null, signal = LTE_SIGNAL, subscriptionId = SUB_ID, network = null)
+            )
+
+            result shouldBe expected
+        }
+    }
+
+}
