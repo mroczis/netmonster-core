ee14dbd09c1a51dae541c2b0d4703797cac20054
handymenny <handymenny@outlook.com>
2022-04-09 19:47:27 +0200

    Filter out invalid NR cells on Mediatek devices
    The following Mediatek devices return fake NR cells with NCI 0 or 268435455, SS/CSI-RSRP = -44 and SS/CSI-RSRP = -3:
    Xiaomi M2007J22G
    Oneplus DN2103
    Oppo CPH2211
    Realme RMX2111
    SDK 29 and 30 (31 and above not tested)

--- library/src/main/java/cz/mroczis/netmonster/core/feature/postprocess/InvalidCellsPostprocessor.kt
+++ library/src/main/java/cz/mroczis/netmonster/core/feature/postprocess/InvalidCellsPostprocessor.kt
@@ -2,9 +2,11 @@ package cz.mroczis.netmonster.core.feature.postprocess
 
 import cz.mroczis.netmonster.core.model.cell.CellGsm
 import cz.mroczis.netmonster.core.model.cell.CellLte
+import cz.mroczis.netmonster.core.model.cell.CellNr
 import cz.mroczis.netmonster.core.model.cell.ICell
 import cz.mroczis.netmonster.core.model.connection.PrimaryConnection
 import cz.mroczis.netmonster.core.util.isHuawei
+import cz.mroczis.netmonster.core.util.isMediatek
 import cz.mroczis.netmonster.core.util.isSamsung
 
 /**
@@ -24,6 +26,9 @@ class InvalidCellsPostprocessor : ICellPostprocessor {
         // - a7y18ltexx, API 28
         // - beyond1lteeea, API 29
         .filterNot { it is CellLte && isSamsung() && it.band?.number == 1 && it.connectionStatus !is PrimaryConnection && it.signal.rsrp != null && it.signal.rsrq == null }
+        // Mediatek devices return these fake NR (primary) cells, in NSA mode, most likely when they attempt to connect to an NR cell or have just disconnected.
+        // The only valid values are PCI, PLMN and NR-ARFCN, NCI is 0 or 268435455 (filtered out by CellMapperNR), RSRP -44 and RSRQ -3
+        .filterNot { it is CellNr && isMediatek() && it.nci == null && it.signal.ssRsrp == -44 && it.signal.ssRsrq == -3 && it.signal.csiRsrp == -44 && it.signal.csiRsrq == -3 }
         .toList()
 
 }
