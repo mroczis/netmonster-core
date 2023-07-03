8af46f73af69e1c91248b3d68b6e19c4939bc31e
Bivek Pandey <pandeyvivak25@hotmail.com>
2023-07-03 13:30:19 +0300

    some customization and mqtt setup

--- app/src/main/java/cz/mroczis/netmonster/sample/MainActivity.kt
+++ app/src/main/java/cz/mroczis/netmonster/sample/MainActivity.kt
@@ -2,6 +2,7 @@ package cz.mroczis.netmonster.sample
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
@@ -12,24 +13,51 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.os.postDelayed
 import cz.mroczis.netmonster.core.factory.NetMonsterFactory
+import cz.mroczis.netmonster.core.feature.merge.CellSource
+import cz.mroczis.netmonster.core.model.cell.ICell
+import cz.mroczis.netmonster.sample.MainActivity.Companion.REFRESH_RATIO
 import cz.mroczis.netmonster.sample.databinding.ActivityMainBinding
+import org.eclipse.paho.client.mqttv3.MqttClient
+
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions
+import org.eclipse.paho.client.mqttv3.MqttException
+import org.eclipse.paho.client.mqttv3.MqttMessage
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
+import kotlin.random.Random
+import java.io.ByteArrayOutputStream
+import java.io.ObjectOutputStream
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 /**
  * Activity periodically updates data (once in [REFRESH_RATIO] ms) when it's on foreground.
  */
-class MainActivity : AppCompatActivity() {
 
+
+class MainActivity : AppCompatActivity() {
+    var context: Context = this
     companion object {
         private const val REFRESH_RATIO = 5_000L
     }
+
 
     private val handler = Handler(Looper.getMainLooper())
     private val adapter = MainAdapter()
 
     private lateinit var binding: ActivityMainBinding
 
+
+//    val brokerUri = "tcp://10.0.2.2:1883" // Replace with your MQTT broker URI
+    private val brokerUri = "tcp://broker.hivemq.com:1883" //
+    private val clientId = "publish-${Random.nextInt(0, 1000)}"
+    private val persistence = MemoryPersistence()
+    private val mqttClient = MqttClient(brokerUri, clientId, persistence)
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         with(binding) {
@@ -38,13 +66,30 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun connectToMqttBroker() {
+        try {
+            val mqttOptions = MqttConnectOptions()
+            mqttOptions.isCleanSession = true
+            mqttClient.connect(mqttOptions)
+            println("connected")
+            println("connected")
+            println("connected")
+            println("connected")
+        } catch (e: MqttException) {
+            e.printStackTrace()
+        }
+    }
+
+
     override fun onResume() {
         super.onResume()
+
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
             == PackageManager.PERMISSION_GRANTED &&
             ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
             == PackageManager.PERMISSION_GRANTED
         ) {
+            connectToMqttBroker()
             loop()
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             requestPermissions(arrayOf(
@@ -53,11 +98,25 @@ class MainActivity : AppCompatActivity() {
                 Manifest.permission.READ_PHONE_STATE
             ), 0)
         }
+
+
     }
 
     override fun onPause() {
         super.onPause()
         handler.removeCallbacksAndMessages(null)
+        disconnectFromMqttBroker()
+
+    }
+
+    private fun disconnectFromMqttBroker() {
+        try {
+            mqttClient.disconnect()
+            println("Mqtt Disconnected")
+        } catch (e: MqttException) {
+            e.printStackTrace()
+            println("Problem to discconnect")
+        }
     }
 
     private fun loop() {
@@ -65,14 +124,33 @@ class MainActivity : AppCompatActivity() {
         handler.postDelayed(REFRESH_RATIO) { loop() }
     }
 
+
     @SuppressLint("MissingPermission")
     private fun updateData() {
+
         NetMonsterFactory.get(this).apply {
             val merged = getCells()
             adapter.data = merged
 
+            val gson = Gson()
+            val mergedJson = gson.toJson(merged)
+
+            println("--------------------------------------------------------")
             Log.d("NTM-RES", " \n${merged.joinToString(separator = "\n")}")
+            println("--------------------------------------------------------")
+
+            val mqttMessage = MqttMessage(mergedJson.toByteArray())
+            mqttMessage.qos = 0
+            mqttMessage.isRetained = false
+            mqttClient.publish("dt/message", mqttMessage)
+
         }
+
+
+
     }
 
+
 }
+
+
